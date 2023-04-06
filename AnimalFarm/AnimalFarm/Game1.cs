e12a5d6e6ec02b5727adf13ec75130cdc2fa0494
Consiglieres(Никита Городецкий) <98966123+consiglieres@users.noreply.github.com>
2023-04-07 00:26:40 +0500

    Add files via upload

---
+++ AnimalFarm/AnimalFarm/Game1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AnimalFarm
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D _backGround;
+        Texture2D _chicken;
+        Texture2D _cow;
+        Texture2D _goat;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _backGround = Content.Load<Texture2D>("grass");
+            _chicken = Content.Load<Texture2D>("chicken");
+            _goat = Content.Load<Texture2D>("goat");
+            _cow = Content.Load<Texture2D>("cow");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+                    
+                
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_backGround, Vector2.Zero, Color.White);
+            _spriteBatch.Draw(_chicken, new Vector2(RandomUnit.RandomChicken(), RandomUnit.RandomChicken()), Color.GhostWhite);
+            _spriteBatch.Draw(_goat, new Vector2(RandomUnit.Goat(), RandomUnit.Goat()), Color.GhostWhite);
+            _spriteBatch.Draw(_cow, new Vector2(RandomUnit.Cow(), RandomUnit.Cow()), Color.GhostWhite);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        
+    }
+}
---
+++ AnimalFarm/AnimalFarm/class/RandomUnit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms.VisualStyles;
+
+namespace AnimalFarm;
+
+public class RandomUnit
+{
+    public static int RandomChicken() 
+    {
+        var x = new Random();
+        var value = x.Next(300, 650);
+        Thread.Sleep(500);
+        return (value);
+    }
+
+    public static void BlackChicken()
+    {
+        var randomX = new Random();
+        var randomY = new Random();
+        return;
+    }
+    
+    public static int Cow()
+    {
+        var x = new Random();
+        var value = x.Next(300, 650);
+        Thread.Sleep(600);
+        return(value);
+    }
+    
+    public static int Goat()
+    {
+        var x = new Random();
+        var value = x.Next(300, 650);
+        Thread.Sleep(600);
+        return(value);
+    }
+}
